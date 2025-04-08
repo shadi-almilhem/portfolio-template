2ab499b5c1405b5e7d1895fc65c500a45a323e5c
Shadi Al Milhem <almilhemshadi@gmail.com>
2025-04-08 20:24:14 +0400

    enhance ThemeToggle button styles and transitions for improved visual feedback

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -16,11 +16,11 @@ export function ThemeToggle() {
     <button
       type="button"
       aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
-      className="group rounded-full bg-white/90 px-2 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+      className="group rounded-full bg-gradient-to-br bg-white/90 px-2 py-2 shadow-xl  ring-1 ring-zinc-900/5 backdrop-blur  dark:bg-zinc-800/90 transition-all duration-300 dark:shadow-emerald-500/30 shadow-emerald-500/10  dark:ring-white/10 dark:hover:ring-white/20"
       onClick={() => setTheme(otherTheme)}
     >
-      <Sun className="h-6 w-6  transition  dark:hidden fill-emerald-50 stroke-emerald-500 group-hover:fill-emerald-50 group-hover:stroke-emerald-600" />
-      <MoonStar className="hidden h-6 w-6 group-hover:stroke-emerald-400  transition dark:block stroke-emerald-500 fill-emerald-400/10 " />
+      <Sun className="size-6  transition-all duration-300  dark:hidden fill-emerald-100 stroke-emerald-500 group-hover:fill-emerald-50 group-hover:stroke-emerald-500/80" />
+      <MoonStar className="hidden size-6 group-hover:stroke-emerald-400   transition-all duration-300 dark:block stroke-emerald-500 !fill-emerald-400/20 " />
     </button>
   );
 }
